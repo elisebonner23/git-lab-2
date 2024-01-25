7b5ec33411cbd7c71a5044f9aa576dff6d578e02
elisebonner23 <156691746+elisebonner23@users.noreply.github.com>
2024-01-24 20:40:05 -0500

    Update git-lab-program.cc

--- git-lab-program.cc
+++ git-lab-program.cc
@@ -1,1 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Elise Bonner
+ *        Date: Wed Jan 24, 2024
+ * Description: Add Description
+ */
 
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << end1;
+    return 0;
+}// main
